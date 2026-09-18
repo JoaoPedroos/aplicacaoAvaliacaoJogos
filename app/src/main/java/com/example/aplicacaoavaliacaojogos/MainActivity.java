742b4cc31c7ad4f54a45ab89c4fbe761e23024e7
Joao <joaopedroenv@gmail.com>
2026-09-18 20:47:14 -0300

    feat:adicionando avaliação do jogo angry bird

---
+++ app/src/main/java/com/example/aplicacaoavaliacaojogos/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.aplicacaoavaliacaojogos;
+
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView txtstatus;
+    RatingBar rtbvotacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        txtstatus = (TextView) findViewById(R.id.txtstatus);
+        rtbvotacao = (RatingBar) findViewById(R.id.rtbvotacao);
+        txtstatus.setText("Status: Ruim");
+
+        rtbvotacao.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                if(rating == 1)
+                    txtstatus.setText("Status: Regular");
+                else if(rating == 2)
+                    txtstatus.setText("Status: Bom");
+                else if(rating <= 3)
+                    txtstatus.setText("Status: Ótimo");
+                else if(rating == 4)
+                    txtstatus.setText("Status: Excelente");
+            }
+        });
+
+
+    }
+}
